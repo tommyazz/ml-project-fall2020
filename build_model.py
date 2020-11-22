3ce665092e53958324626b6a4a061d68b578429c
Tommy Azzino <tommy.azzino@gmail.com>
2020-11-22 00:43:18 -0500

    Add GRU model

--- build_model.py
+++ build_model.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 
 import tensorflow as tf
-# complete with all the necessary imports
+from tensorflow.keras.layers import Input, Embedding, Dense, Flatten, Dropout, GRU
+from tensorflow.keras.models import Model
 
 '''
 Define functions for creating a specific type of network architecture.
@@ -12,29 +13,37 @@ A second architecture (based on 3D CNN) will additionally exploit camera images 
 # [1] https://arxiv.org/abs/2002.02445; Github: https://github.com/malrabeiah/VABT/tree/master
 
 # Create a method that returns the first model using keras APIs
-def build_gru_model(input_size, output_size, embed_size, hidden_size, num_layers):
-    model = Sequential()
+def build_gru_model(input_size, codebook_size, embed_size=50, hidden_size=20, num_layers=2, dropout=0.2):
+
+    inputs = Input(shape=(input_size,), name="input_layer")
     # Add an embedding layer (as discussed in paper [1]): https://keras.io/api/layers/core_layers/embedding/
     # The output of the embedding layer should have shape: [batch_size, input_size, embed_size]
-    model.add()
+    embedding = Embedding(codebook_size+1, embed_size, name="embedding_layer")(inputs)
 
     # Add "num_layers" GRU layers with "hidden_size" units. Use the parameters provided in [1]
     # https://keras.io/api/layers/recurrent_layers/gru/
+    layer_output = embedding
     for i in range(num_layers):
-        model.add()
-        # Note: the last layer should have "return_sequences": True, in order to take the output of the last layer. See doc.
-        # The final output should have shape [batch_size, hidden_size]. Check with model.summary(), I might be wrong.
+        if i+1 == num_layers:
+            layer_output = GRU(hidden_size, return_sequences=False, 
+                               dropout=dropout, name="recurrent_layer_"+str(i+1))(layer_output)
+        else:
+            layer_output = GRU(hidden_size, return_sequences=True, 
+                               dropout=dropout, name="recurrent_layer_"+str(i+1))(layer_output)
+        # Note: the last layer should have "return_sequences": False, in order to take the output of the last layer. See doc.
+        # The final output should have shape [batch_size, hidden_size].
     
     # In the paper, they create a custom layer to extract the output given the # of beams to predict (N).
     # Since for the moment we will focus on the prediction of the first future beam, we shouldn't need to create a similar layer.
 
     # Add a Dense layer with dimension output_size (i.e. codebook size) and "linear" or "relu" activation function (not clear in their code). Probably relu. 
-    model.add()
+    out = Dense(codebook_size, activation='softmax')(layer_output)
 
     # Add Softmax activation layer. Be careful: this is not another Dense layer with activation function "softmax" (introduces
     # new training parameters), but just a layer which computes softmax activation on the output of the previous layer.
     # See Keras Doc.
 
+    model = Model(inputs=inputs, outputs=out)
     return model
 
 
--- train_model.py
+++ train_model.py
@@ -4,29 +4,54 @@
 # [1] https://arxiv.org/abs/2002.02445;
 # Using the functions you defined in "utils" and build_model.py, create code to train the model.
 
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from sklearn.preprocessing import OneHotEncoder
 from utils.data_loader import load_beam_data
 from build_model import build_gru_model
 
+
 # load training, validation and test data (note: there's no need to scale the data)
 train_path = "./dev_dataset_csv/train_set.csv"
+val_path = "./dev_dataset_csv/val_set.csv"
 Xtr, ytr = load_beam_data(train_path)
-print(Xtr.shape)
-print(ytr.shape)
+Xval, yval = load_beam_data(val_path)
+print(f"Training data shape: {Xtr.shape}")
+print(f"Validation data shape: {Xval.shape}")
+# One-hot-encoding of training and val target
+enc = OneHotEncoder()
+enc.fit_transform(np.vstack((ytr, [0]))) # needed to manually add codeword "0" in order to one-hot-code to the correct codebook size
+# It seems codeword corresponding to index 0 has not been collected in the data
+ytr_e = enc.transform(ytr).toarray()
+yval_e = enc.transform(yval).toarray()
+print(f"Encoded training target shape: {ytr_e.shape}")
+print(f"Encoded validation target shape: {yval_e.shape}")
 
 # create the model, print a summary to check all the parameters
-# model = build_gru_model()
+K.clear_session()
+input_size = Xtr.shape[1]
+codebook_size = np.max(Xtr)+1
+print(codebook_size)
+model = build_gru_model(input_size, int(codebook_size))
+print(model.summary())
 
 # compile the model with proper optimizer (Adam(lr=0.001)) and loss function 
-# model.compile()
+opt = Adam(lr=0.001)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 # define the following callbacks:
 # - model_checkpoint: https://keras.io/api/callbacks/model_checkpoint/ (read doc and understand its function)
 # - (optional) reduce_lr: https://keras.io/api/callbacks/reduce_lr_on_plateau/ (read doc and understand its function)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.00001, verbose=1)
 # - any other custom or available callback you think might be useful.
 
 # fit model on train data using batch_size and epochs as in paper [1]. Use also the callbacks you defined.
 # https://keras.io/api/models/model_training_apis/
-# train_hist = model.fit()
+hist = model.fit(Xtr, ytr_e, validation_data=(Xval, yval_e), 
+                 batch_size=1000, epochs=100, callbacks=[reduce_lr])
 
 # plot training statistics. 
 
