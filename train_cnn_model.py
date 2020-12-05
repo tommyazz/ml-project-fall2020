bbcdfa6a42290a3c100b9816b5f21767c3f8293c
tommyazz <tommy.azzino@nyu.edu>
2020-12-05 18:25:39 -0500

    Using tensorflow for loading images

--- train_cnn_model.py
+++ train_cnn_model.py
@@ -9,7 +9,6 @@ import pickle
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.callbacks import ReduceLROnPlateau, LearningRateScheduler, ModelCheckpoint
 from sklearn.preprocessing import OneHotEncoder
 from utils.data_loader import *
@@ -37,17 +36,18 @@ class CustomDataGenerator(tf.keras.utils.Sequence):
         return self.__getdata__(indexes)
 
     def __getdata__(self, indexes):
-        beam_batch = np.zeros((self.batch_size, self.hist_size))
-        image_batch = np.zeros((self.batch_size, self.hist_size, self.im_size[0], self.im_size[1], self.im_size[2]))
-        y_batch = np.zeros((self.batch_size, self.num_classes))
+        beam_batch = tf.zeros((self.batch_size, self.hist_size), dtype=tf.dtypes.float16)
+        image_batch = tf.zeros((self.batch_size, self.hist_size, self.im_size[0], self.im_size[1], self.im_size[2]),
+                               dtype=tf.dtypes.float16)
+        y_batch = tf.zeros((self.batch_size, self.num_classes), dtype=tf.dtypes.int32)
+        scaling = tf.constant([255], dtype=tf.dtypes.float16)
         for i,idx in enumerate(indexes):
             y_batch[i,:] = self.y[idx,:]
             beam_batch[i,:] = self.X[idx, 0:self.hist_size]
             for j in range(self.hist_size):
-                im = image.load_img(self.X[idx, self.hist_size+j], target_size=(self.im_size[0], self.im_size[1]))
-                im_a = image.img_to_array(im, data_format="channels_last", dtype=np.float16)
-                im.close()
-                image_batch[i,j,:,:,:] = im_a/255.0
+                img = tf.image.decode_jpeg(tf.io.read_file(self.X[idx, self.hist_size+j])))
+                img = tf.image.resize(img, [self.im_size[0], self.im_size[1]])
+                image_batch[i,j,:,:,:] = tf.divide(img, scaling)
     
         return [beam_batch, image_batch], y_batch
 
